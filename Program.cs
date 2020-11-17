55387cfdb9917bef0392a0ceaf53878329b33d76
Maiara Evangelista <maiaraevangelista933@gmail.com>
2020-11-17 16:43:34 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Projeto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deseja ver a taboada de qual numero? ");
+            int resposta = int.Parse(Console.ReadLine());
+
+            int resultado;
+
+            for (int contador = 10; contador >= 0; contador --)
+            {
+               // Console.WriteLine(contador);
+              //  System.Threading.Thread.Sleep(50);
+              resultado = resposta * contador;
+              Console.WriteLine($"{resposta} * {contador} = {resposta * contador}");
+            }
+        }
+    }
+}
